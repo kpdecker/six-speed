c09e6d9e44c82a70ff2a82e32f3583f16460d141
kpdecker <kpdecker@gmail.com>
2015-06-23 17:17:06 -0500

    Support exec under node 0.*

--- gulpfile.js
+++ gulpfile.js
@@ -15,20 +15,23 @@ require('./tasks/server');
 
 Gulp.task('test:node', ['build:tests'], function(callback) {
   ChildProcess.exec('node  --v8-options | grep "in progress"', function(err, stdout) {
-    if (err) {
+    if (err && err.code !== 1) {
       throw new GUtil.PluginError('test:node', err);
     }
 
     // Run with everything enabled, per https://iojs.org/en/es6.html
-    var args = stdout.replace(/\n$/, '').split(/\n/g).map(function(line) {
+    var args = _.compact(stdout.replace(/\n$/, '').split(/\n/g).map(function(line) {
       if (/(--\w+)/.exec(line)) {
         return RegExp.$1;
-      } else {
-        return '';
       }
-    });
-    args.push('--es_staging');
+    }));
+    if (/^0/.test(process.versions.node)) {
+      args.push('--harmony');
+    } else {
+      args.push('--es_staging');
+    }
     args.push('lib/node');
+    console.log(args);
 
     var test = ChildProcess.spawn('node', args, {stdio: 'inherit'});
     test.on('close', function(code) {
