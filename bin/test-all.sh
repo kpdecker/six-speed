601df42425e792abd3809ef5648acaf971ced351
kpdecker <kpdecker@gmail.com>
2015-10-05 21:04:08 -0500

    Add delete-prefix nvm command to work around probable El Capitan bug
    https://github.com/creationix/nvm/issues/855

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -14,9 +14,9 @@ nvm install 4
 ./node_modules/.bin/gulp test:local
 ./node_modules/.bin/gulp test:sauce
 
-nvm use 0.12
+nvm --delete-prefix use 0.12
 ./node_modules/.bin/gulp test:node
 
-nvm use 4
+nvm --delete-prefix use 4
 ./node_modules/.bin/gulp test:node
 
