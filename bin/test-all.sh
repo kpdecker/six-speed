c41920a76d4e54e4bd7e6aac3775f37a54d89848
kpdecker <kpdecker@gmail.com>
2015-10-06 19:30:11 -0500

    Bulletproof testall
    Handle node version changes as well as lingering sc instances.

--- bin/test-all.sh
+++ bin/test-all.sh
@@ -1,22 +1,25 @@
 . ~/.nvm/nvm.sh
+killall sc
 
-npm update
+nvm install 4
+
+rm -rf node_modules
+npm install
 
 rm -rf browsers
 mkdir browsers
 ./node_modules/.bin/browser-downloader browsers
-
-nvm install 0.12
-nvm install 4
 
 ./node_modules/.bin/gulp build
 
 ./node_modules/.bin/gulp test:local
 ./node_modules/.bin/gulp test:sauce
 
-nvm use --delete-prefix 0.12
 ./node_modules/.bin/gulp test:node
 
-nvm use --delete-prefix 4
-./node_modules/.bin/gulp test:node
+nvm install 0.12
 
+rm -rf node_modules
+npm install
+
+./node_modules/.bin/gulp test:node
