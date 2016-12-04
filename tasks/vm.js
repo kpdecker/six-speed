2461c6dc86c563c634bda72a1210e0c2959dd6b7
kpdecker <kpdecker@gmail.com>
2016-12-04 10:05:47 -0600

    Fix VM path

--- tasks/vm.js
+++ tasks/vm.js
@@ -13,7 +13,7 @@ Gulp.task('test:vm:edge', ['build:browser'], function(callback) {
 
 
 function runVM(run, callback) {
-  var vmx = './browsers/MsEdge-Win10TH2-VMware.vmwarevm';
+  var vmx = './browsers/MSEdge - Win10_preview.vmx';
   Server.start(function(uri) {
     loadSnapshot(vmx)
         .then(function() { return startVM(vmx); })
