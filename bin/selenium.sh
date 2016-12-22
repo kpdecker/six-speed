7a85ea53e60a3687fd1bfeb0d288ec9327aad462
kpdecker <kpdecker@gmail.com>
2016-12-21 20:40:06 -0600

    Remove selenium scripts

--- bin/selenium.sh
+++
@@ -1,8 +0,0 @@
-JAR_FILE=./bin/selenium-server-standalone-2.45.0.jar
-CHROME_DRIVER=./bin/chromedriver
-
-java \
-    -Dwebdriver.chrome.driver=$CHROME_DRIVER \
-    -DSafariDefaultPath=/Applications/WebKit.app/Contents/MacOS/WebKit \
-    -Dwebdriver.firefox.bin=/Applications/FirefoxNightly.app/Contents/MacOS/firefox-bin \
-    -jar $JAR_FILE
